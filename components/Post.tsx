fbc5df000ff0f600384e833ab254d083fe3504d7
rezwanahammad <127617637+rezwanahammad@users.noreply.github.com>
2025-05-06 20:02:26 +0600

    changes made

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Post = () => {
   return (
-    <div>
-      
+    <div className="p-4 border-y-[1px] border-borderGray">
+      <div className="flex items-center gap-2 text-sm text-textGray mb-2 from-bold">
+        icon
+        <span>HB Posted </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
